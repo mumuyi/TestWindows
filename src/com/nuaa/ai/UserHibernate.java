8a729f96a109c34520f1bed2fc7c09db251b8fd6
root <1289167109@qq.com>
2016-12-31 22:13:02 +0800

    增加了表格的上下翻页功能,之后需要看看并加上总页数和当前页数的显示.

--- src/com/nuaa/ai/UserHibernate.java
+++ src/com/nuaa/ai/UserHibernate.java
@@ -3,14 +3,16 @@ package com.nuaa.ai;
 import java.util.Iterator;
 import java.util.List;
 
+import org.hibernate.Criteria;
 import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.cfg.Configuration;
+import org.hibernate.criterion.Projections;
 import org.hibernate.SessionFactory;
 
-public class HibernateTest {
+public class UserHibernate {
 
-	public static List<User> UserQuery() {
+	public static List<User> UserQuery(int from,int max) {
 
 		Configuration cfg = new Configuration();
 		// cfg.configure();可带参数指定配置文件.返回值还是一个configuration 但是其拥有了配置选项;
@@ -22,6 +24,12 @@ public class HibernateTest {
 		session.beginTransaction();
 		// 参数是一个字符串,是HQL的查询语句.注意此时的的UserU为大写,为对象的,而不是表的.
 		Query query = session.createQuery("from User");
+		
+        //从第一个开始查起.可以设置从第几个查起.
+        query.setFirstResult(from);
+        //最大条数为两个
+        query.setMaxResults(max);
+		
 		// 使用List方法.
 		List<User> userList = query.list();
 		// 迭代器去迭代.
@@ -35,5 +43,26 @@ public class HibernateTest {
 		session.close();
 		sf.close();
 		return userList;
+	}
+	
+	public static long UserGetRecordNum(){
+		Configuration cfg = new Configuration();
+		// cfg.configure();可带参数指定配置文件.返回值还是一个configuration 但是其拥有了配置选项;
+		SessionFactory sf = cfg.configure().buildSessionFactory();
+		// 打开session;
+		Session session = sf.openSession();
+		// 开始事务;
+		session.beginTransaction();
+		Query query = session.createQuery("select count(*) from User");
+		// 获取事务并提交;
+		session.getTransaction().commit();
+		//获取长度;
+		long sum=(long) query.uniqueResult();
+		session.close();
+		sf.close();
+		return sum;
+		//Criteria criteria = session.createCriteria(User.class);
+		//criteria.setProjection(Projections.rowCount());
+		//return criteria.list().size();
 	}
 }
