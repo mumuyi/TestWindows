02b46f5429d7e8cf0a4831b736dd77f51a4f3571
root <1289167109@qq.com>
2017-01-02 12:52:17 +0800

    添加了下拉选择框和一个删除时的提示框

--- src/com/nuaa/ai/StudentTableWindow.java
+++ src/com/nuaa/ai/StudentTableWindow.java
@@ -6,9 +6,9 @@ public class StudentTableWindow {
 	
 	public void iniTable(){
 		StudentTableWindow utw=new StudentTableWindow();
-		String[] Titles = { "ID", "Name" };
+		String[] Titles = { "±àºÅ","ID", "Name","Ñ¡Ôñ" };
 		@SuppressWarnings("unchecked")
-		List<Student> list = (List<Student>)MyHibernate.sqlQuery(0, 2,"from Student");
+		List<Student> list = (List<Student>)MyHibernate.sqlQuery(0, 4,"from Student");
 		TableWindowsFactory table=new TableWindowsFactory("Student Table",Titles,list,utw,MyHibernate.sqlGetRecordNum("select count(*) from Student"));
 		table.showTable();
 	}
@@ -17,5 +17,8 @@ public class StudentTableWindow {
 	public List<Student> update(int from,int maxLineATime){
 		return (List<Student>)MyHibernate.sqlQuery(from, maxLineATime,"from Student");
 	}
-
+	public static void main(String[] args){
+		StudentTableWindow stw=new StudentTableWindow();
+		stw.iniTable();
+	}
 }
--- src/com/nuaa/ai/TableWindowsFactory.java
+++ src/com/nuaa/ai/TableWindowsFactory.java
@@ -1,13 +1,13 @@
 package com.nuaa.ai;
 
 import java.awt.BorderLayout;
+import java.awt.Choice;
 import java.awt.Color;
 import java.awt.Component;
 import java.awt.Font;
 import java.awt.TextField;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.lang.reflect.Field;
@@ -15,26 +15,20 @@ import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.Date;
-import java.util.LinkedList;
 import java.util.List;
 
 import javax.swing.BorderFactory;
-import javax.swing.DefaultCellEditor;
 import javax.swing.JButton;
-import javax.swing.JCheckBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
-import javax.swing.event.TableModelEvent;
-import javax.swing.event.TableModelListener;
 import javax.swing.table.DefaultTableCellRenderer;
-import javax.swing.table.TableCellRenderer;
 import javax.swing.table.TableColumn;
-import javax.swing.table.TableColumnModel;
 
-public class TableWindowsFactory implements ActionListener ,MouseListener{
+public class TableWindowsFactory implements ActionListener, MouseListener {
 
 	private JFrame frame = null;
 	private JTable table = null;
@@ -59,16 +53,19 @@ public class TableWindowsFactory implements ActionListener ,MouseListener{
 	private List<?> sqlList;
 	// 调用的类;
 	private Object object;
-	//搜索条件输入框;
+	// 搜索条件输入框;
 	private TextField text;
-	//搜索条件;
-	private String searchCondition=null;
+	// 搜索条件;
+	private String searchCondition = null;
 	// 特殊行数据;
 	private List<Integer> specialrow = new ArrayList<>();
-	//表头;
+	// 表头;
 	private String[] TableTitles;
-	//需要删除的数据;
-	private List<Integer> delList=new ArrayList<Integer>();
+	// 需要删除的数据;
+	private List<Integer> delList = new ArrayList<Integer>();
+	// 下拉选择框;
+	private Choice choice;
+
 	// 构造函数;
 	/**
 	 * TableTitle 窗口名; TableTitles 表头; objectList 表中第一页的数据; formerObject
@@ -84,8 +81,8 @@ public class TableWindowsFactory implements ActionListener ,MouseListener{
 		object = formerObject;
 		sqlList = objectList;
 		this.totalNum = totalNum;
-		this.TableTitles=TableTitles;
-		
+		this.TableTitles = TableTitles;
+
 		// 初始化首页数据;
 		for (int i = 0; i < sqlList.size(); i++) {
 			try {
@@ -97,11 +94,17 @@ public class TableWindowsFactory implements ActionListener ,MouseListener{
 			model.addRow(list);
 		}
 
-		//设置第一列行号的宽度为固定值;
+		// 设置第一列行号的宽度为固定值;
 		TableColumn firsetColumn = table.getColumnModel().getColumn(0);
 		firsetColumn.setPreferredWidth(30);
 		firsetColumn.setMaxWidth(30);
 		firsetColumn.setMinWidth(30);
+
+		// 设置最后一列行号的宽度为固定值;
+		TableColumn lastColumn = table.getColumnModel().getColumn(TableTitles.length - 1);
+		lastColumn.setPreferredWidth(50);
+		lastColumn.setMaxWidth(50);
+		lastColumn.setMinWidth(50);
 
 		// 增加测试数据,测试表格中对特殊行显示特殊的颜色;
 		// specialrow.add(0);
@@ -116,20 +119,20 @@ public class TableWindowsFactory implements ActionListener ,MouseListener{
 
 		// 给表格增加颜色;
 		makeFace(table, specialrow);
-		
-		//添加表格单元格点击事件;
+
+		// 添加表格单元格点击事件;
 		table.addMouseListener(this);
 
 		// 将表格加入窗口中;
 		s_pan = new JScrollPane(table);
 		frame.add(s_pan);
 
-		//添加按钮和页数信息;
+		// 添加按钮和页数信息;
 		addBotton();
 
-		//添加搜索框;
+		// 添加搜索框;
 		addSearch();
-		
+
 		// 设置窗口大小;
 		frame.setSize(600, 400);
 
@@ -154,20 +157,28 @@ public class TableWindowsFactory implements ActionListener ,MouseListener{
 
 				public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected,
 						boolean hasFocus, int row, int column) {
-					//设置cell 中的数据居中显示;
+					// 设置cell 中的数据居中显示;
 					setHorizontalAlignment((int) 0.5f);
-					
+
 					// 设置相邻两行的颜色不同,方便用户查看;
 					if (row % 2 == 0) {
 						setBackground(Color.white); // 设置奇数行底色
 					} else if (row % 2 == 1) {
 						setBackground(Color.GRAY); // 设置偶数行底色
 					}
-					// 如果需要设置某一个Cell颜色，需要加上column过滤条件即可
-					 if (searchCondition!=null&&value.toString().contains(searchCondition)&&column!=0) {
-						 setBackground(Color.YELLOW); 
-					 }
-					 
+					// 搜索;
+					// 当前单元格包含所有字段;除去了编号和复选框字段;
+					// 如果搜索的条件是空的话,直接显示,不上特殊的颜色;
+					if (searchCondition != null && !searchCondition.isEmpty() 
+							&& column != 0&& column != TableTitles.length 
+							&& value.toString().contains(searchCondition)) {
+						// 列选条件;
+						// System.out.println(""+choice.getSelectedIndex()+"
+						// "+column);
+						if (choice.getSelectedIndex() == 0 || choice.getSelectedIndex() == column)
+							setBackground(Color.YELLOW);
+					}
+
 					// 设置特定行的颜色;将所有需要特殊化处理的行号加入specialrow
 					// 中,之后再判断当前处理的行号是否包含在specialrow 中即可;
 					if (specialrow.contains(row)) {
@@ -181,8 +192,8 @@ public class TableWindowsFactory implements ActionListener ,MouseListener{
 					return super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
 				}
 			};
-			//在这里不做最后一项,在用最开始的方法即可设置成可选的复选框,但是依旧是没有上色的.
-			for (int i = 0; i < table.getColumnCount()-1; i++) {
+			// 在这里不做最后一项,在用最开始的方法即可设置成可选的复选框,但是依旧是没有上色的.
+			for (int i = 0; i < table.getColumnCount() - 1; i++) {
 				table.getColumn(table.getColumnName(i)).setCellRenderer(tcr);
 			}
 
@@ -192,9 +203,8 @@ public class TableWindowsFactory implements ActionListener ,MouseListener{
 
 	}
 
-	
-	//添加按钮和页数;
-	private void addBotton(){
+	// 添加按钮和页数;
+	private void addBotton() {
 		// 创建JPanel;
 		JPanel buttonPanel = new JPanel();
 		buttonPanel.setBorder(BorderFactory.createEmptyBorder(4, 4, 4, 4));
@@ -221,36 +231,48 @@ public class TableWindowsFactory implements ActionListener ,MouseListener{
 
 		// 第一页时设置上一页按钮不可用;
 		lastPagebutton.setEnabled(false);
+
+		// 如果只有一列,也要设置下一列按钮不可用;
+		if ((currentPage + 1) * maxLineATime >= totalNum)
+			nextPagebutton.setEnabled(false);
 	}
-	
-	private void addSearch(){
+
+	private void addSearch() {
 		// 创建JPanel;
 		JPanel searchPanel = new JPanel();
 		searchPanel.setBorder(BorderFactory.createEmptyBorder(4, 4, 4, 4));
-		
-		//添加输入框;
+
+		// 添加输入框;
 		text = new TextField();
-		//text.setBounds(0, 0, 200, 5);
+		// text.setBounds(0, 0, 200, 5);
 		searchPanel.add(text);
-		
+
 		// 添加搜索按钮;
 		JButton searchButton = new JButton("Search");
 		searchButton.setActionCommand("Search");
 		searchButton.addActionListener(this);
 		searchPanel.add(searchButton);
-		
+
+		// 添加下拉选择框;
+		choice = new Choice();
+		choice.add("所有列");
+		// 不添加第一列和最后一列;(其中第一列为编号,最后一列为复选框);
+		for (int i = 1; i < TableTitles.length - 1; i++) {
+			choice.add(TableTitles[i]);
+		}
+		searchPanel.add(choice);
+		// choice.getSelectedItem()
+		// choice.getSelectedIndex()
 		// 添加删除按钮;
 		JButton delButton = new JButton("Delect");
 		delButton.setActionCommand("delect");
 		delButton.addActionListener(this);
 		searchPanel.add(delButton, BorderLayout.WEST);
-		
+
 		// 将JPanel 添加到窗口中;
 		frame.add(searchPanel, BorderLayout.NORTH);
 	}
-	
-	
-	
+
 	// 鼠标监听事件;
 	@Override
 	public void actionPerformed(ActionEvent e) {
@@ -275,7 +297,7 @@ public class TableWindowsFactory implements ActionListener ,MouseListener{
 			}
 		} else if (e.getActionCommand().equals("Search")) {
 			System.out.println("在这里进行搜索");
-			searchCondition=text.getText();
+			searchCondition = text.getText();
 			// 给表格增加颜色;
 			makeFace(table, specialrow);
 			try {
@@ -284,25 +306,58 @@ public class TableWindowsFactory implements ActionListener ,MouseListener{
 				// TODO Auto-generated catch block
 				e1.printStackTrace();
 			}
-		}else if(e.getActionCommand().equals("delect")){
+		} else if (e.getActionCommand().equals("delect")) {
 			System.out.println("在这里进行删除操作");
-			
-			//删除delList中的对应的sqlList中的对象;
-			for(int i=0;i<delList.size();i++){
-				MyHibernate.sqlDelete(sqlList.get(delList.get(i)));
-				System.out.println(""+delList.get(i));
+
+			//添加一个确认框;
+			int option=JOptionPane.showConfirmDialog( null , "你确定要删除所选择的数据吗" , "提示", JOptionPane.YES_NO_OPTION ) ;
+			if(option==0){
+				// 删除delList中的对应的sqlList中的对象;
+				for (int i = 0; i < delList.size(); i++) {
+					MyHibernate.sqlDelete(sqlList.get(delList.get(i)));
+					System.out.println("" + delList.get(i));
+				}
+	
+				// 更新表格;
+				try {
+					updateTable();
+				} catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+			}			
+		}
+	}
+
+	// 点击事件;
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		// TODO Auto-generated method stub
+		// 获取行列;
+		int[] columns = table.getSelectedColumns();
+		int column = columns[0];
+		int row = table.getSelectedRow();
+		// System.out.println("!!!!!!!!!!"+row+" "+column);
+		// 改变复选框的选择状态,并添加或删除delList中的数据;
+		if (column == TableTitles.length - 1) {
+			// 选中->未选中;
+			if ((Boolean) table.getValueAt(row, column)) {
+				table.setValueAt(false, row, column);
+
+				for (int i = 0; i < delList.size(); i++) {
+					if (delList.get(i) == row)
+						delList.remove(i);
+				}
+
 			}
-			
-			//更新表格;
-			try {
-				updateTable();
-			} catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException e1) {
-				// TODO Auto-generated catch block
-				e1.printStackTrace();
+			// 未选中->选中;
+			else {
+				table.setValueAt(true, row, column);
+				delList.add(row);
 			}
 		}
 	}
-	
+
 	// 数据更新操作;
 	private void updateTable() throws IllegalAccessException, IllegalArgumentException, InvocationTargetException {
 		// 删除model中的之前的数据;
@@ -490,56 +545,28 @@ public class TableWindowsFactory implements ActionListener ,MouseListener{
 		}
 	}
 
-	//点击事件;
-	@Override
-	public void mouseClicked(MouseEvent e) {
-		// TODO Auto-generated method stub
-		//获取行列;
-        int []columns = table.getSelectedColumns();
-        int column=columns[0];
-        int row = table.getSelectedRow();
-        //System.out.println("!!!!!!!!!!"+row+"      "+column);
-        //改变复选框的选择状态,并添加或删除delList中的数据;
-        if(column==TableTitles.length-1){
-        	if((Boolean)table.getValueAt(row, column)){
-        		table.setValueAt(false, row, column);
-        		
-				for(int i=0;i<delList.size();i++){
-				    if(delList.get(i)==row)
-				    	delList.remove(i);
-				}
-        		
-        	}
-        	else{
-        		table.setValueAt(true, row, column);
-        		delList.add(row);
-        	}
-        }
-	}
-
 	@Override
 	public void mousePressed(MouseEvent e) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void mouseReleased(MouseEvent e) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void mouseEntered(MouseEvent e) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void mouseExited(MouseEvent e) {
 		// TODO Auto-generated method stub
-		
-	}
 
+	}
 
 }
